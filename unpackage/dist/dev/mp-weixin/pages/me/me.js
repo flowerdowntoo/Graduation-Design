4991930b8f8e0b4dd496b58b7f739cef31167b80
hxb <2692750313@qq.com>
2022-11-08 22:57:46 +0800

    完成了tabbar功能

---
+++ unpackage/dist/dev/mp-weixin/pages/me/me.js
@@ -0,0 +1,13 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Hbuilder/workSpace/\u5783\u573E\u5206\u7C7B\u4E0E\u56DE\u6536\u7CFB\u7EDF/pages/me/me.vue"]]);
+wx.createPage(MiniProgramPage);
