56d8fd5495c1de2ef9399da5ebc6c61dafd2481c
hxb <2692750313@qq.com>
2022-11-18 16:41:45 +0800

    切换分支之前暂存变动

--- unpackage/dist/dev/mp-weixin/pages/home/home.js
+++ unpackage/dist/dev/mp-weixin/pages/home/home.js
@@ -9,5 +9,5 @@ const _sfc_main = {
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {};
 }
-var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Hbuilder/workSpace/\u5783\u573E\u5206\u7C7B\u4E0E\u56DE\u6536\u7CFB\u7EDF/pages/home/home.vue"]]);
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Hbuilder/workSpace/biyesheji/pages/home/home.vue"]]);
 wx.createPage(MiniProgramPage);
--- unpackage/dist/dev/mp-weixin/pages/me/me.js
+++ unpackage/dist/dev/mp-weixin/pages/me/me.js
@@ -18,5 +18,5 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     })
   };
 }
-var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Hbuilder/workSpace/\u5783\u573E\u5206\u7C7B\u4E0E\u56DE\u6536\u7CFB\u7EDF/pages/me/me.vue"]]);
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Hbuilder/workSpace/biyesheji/pages/me/me.vue"]]);
 wx.createPage(MiniProgramPage);
